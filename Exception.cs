2a8d8ef7babf36b386c24367b198d475b24f9ec7
Jen2006 <jenishacastelino8@gmail.com>
2025-09-18 09:55:52 +0530

    Create Exception.cs
    exception handling

---
+++ Exception.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Exception
+{
+    static void Main()
+{
+
+
+      try
+        {
+            Console.WriteLine("Enter first number");
+            int a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");   
+            int b=Convert.ToInt32(Console.ReadLine());
+
+            int c =a/ b;
+            Console.WriteLine("Result: " + c);
+      }
+
+        catch(DivideByZeroException)
+       {
+            Console.WriteLine("Error:Cannot divide by zero!");
+       }
+       catch(FormatException)
+       {
+           Console.WriteLine("Error:Please enter valid numbers only.");
+        }
+       
+    }
+}    
+     
