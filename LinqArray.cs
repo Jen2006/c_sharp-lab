269b9504faf9e0a92918e863dc42fe93189f8933
Jen2006 <jenishacastelino8@gmail.com>
2025-10-30 10:19:42 +0530

    Implement LINQ to filter even numbers from array
    Added LINQ query to filter even numbers from an array.

--- LinqArray.cs
+++ LinqArray.cs
@@ -1,5 +1,23 @@
 using System;
+using System.Linq;
 
+public class Program 
+{
+    
+    public static void Main(string[] args)
+    {
+        int[] array = { 11,67,24,8,24,78,987,781,223 };
+
+        var result = from n in array
+                     where n % 2 ==0
+                     select n;
+
+        foreach ( var k in result )
+        {
+                       Console.WriteLine(k);
+        }
+    }
+}
 public class Program 
 {
     
